94593cf71321f15b7a9ff4c37192c2aa28afd3cd
Emterry <emma.terry@digital.justice.gov.uk>
2026-07-28 16:22:29 +0100

    white space

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 locals {
-  descheduler-version = "0.33.0"
+  descheduler-version = "0.34.0"
 }
+
 
 resource "helm_release" "descheduler" {
   name       = "descheduler"
@@ -9,7 +10,7 @@ resource "helm_release" "descheduler" {
   namespace  = "kube-system"
   version    = local.descheduler-version
 
-    values = [templatefile("${path.module}/templates/descheduler.yaml.tpl", {
-               enable_removeduplicates  =    var.enable_removeduplicates
+  values = [templatefile("${path.module}/templates/descheduler.yaml.tpl", {
+    enable_removeduplicates = var.enable_removeduplicates
   })]
 }
